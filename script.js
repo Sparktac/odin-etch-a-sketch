509be361d9782543c55dd1b469ea54bad85ce6ac
Dan S <dspark0609@gmail.com>
2022-07-11 15:41:58 -0700

    Remove unnecessary consolelogs from functions

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const containerDiv = document.querySelector('#container')
+const gridItemDiv = document.querySelectorAll('div.grid-item');
 let gridCount = 0;
 
 function makeGrid(rows, columns) {
     containerDiv.style.setProperty('--grid-rows', rows)
     containerDiv.style.setProperty('--grid-columns', columns)
-    console.log(containerDiv);
-    console.log(rows);
-    console.log(columns);
     for (i = 0; i < (rows * columns); i++) {
         let box = document.createElement('div');
-        box.innerText = (i + 1);                //Used to number or label boxes remove when unneeded
-        containerDiv.appendChild(box).className = 'grid-item';
+        box.innerText = (i + 1);
+        containerDiv.appendChild(box).classList.add('grid-item');
     }
 }
 
 makeGrid(16, 16);
 
 function chngBgColor() {
-    let colorBox = document.querySelectorAll('div.grid-item');
-    colorBox.style.setProperty('background-color', 'lightblue')
+    gridItemDiv.style.setProperty('background-color', 'lightblue')
 }
 
 
-makeGrid.addEventListener('mouseover', chngBgColor);
+// gridItemDiv.addEventListener('mouseover', chngBgColor);
