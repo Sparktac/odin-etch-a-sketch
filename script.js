a51785626c0970b1d82dbc6d7e2b81a269c6a6ea
Dan S <dspark0609@gmail.com>
2022-07-11 15:26:03 -0700

    Style css + create grid/color functions

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const containerDiv = document.querySelector('#container')
 let gridCount = 0;
 
-while (gridCount < 32) {
-    const gridDivs = document.createElement('div');
-    gridDivs.classList.add('content');
-    gridDivs.textContent = 'Hello!';
-    containerDiv.appendChild(gridDivs);
-    gridCount++;
-    console.log(gridCount);
+function makeGrid(rows, columns) {
+    containerDiv.style.setProperty('--grid-rows', rows)
+    containerDiv.style.setProperty('--grid-columns', columns)
+    console.log(containerDiv);
+    console.log(rows);
+    console.log(columns);
+    for (i = 0; i < (rows * columns); i++) {
+        let box = document.createElement('div');
+        box.innerText = (i + 1);                //Used to number or label boxes remove when unneeded
+        containerDiv.appendChild(box).className = 'grid-item';
+    }
 }
+
+makeGrid(16, 16);
+
+function chngBgColor() {
+    let colorBox = document.querySelectorAll('div.grid-item');
+    colorBox.style.setProperty('background-color', 'lightblue')
+}
+
+
+makeGrid.addEventListener('mouseover', chngBgColor);
