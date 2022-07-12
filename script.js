0d6127515f9993cc168a109fd328f8282e73b2cd
Dan S <dspark0609@gmail.com>
2022-07-12 14:33:05 -0700

    Remove func call & change CSS container width

--- script.js
+++ script.js
@@ -19,21 +19,23 @@ function makeGrid(rows, columns) {
             box.style.backgroundColor = "lightblue";
         })
     }
-    promptUser();
 }
 
 makeGrid(16, 16);
 
 function promptUser() {
-    button.addEventListener('click', () => {
+    //button.addEventListener('click', () => {
         document.querySelectorAll('.grid-item').forEach(e => e.remove());
         let userInput = prompt('Please enter the number of grid squares per side (Max: 100): ');
         if (userInput > 100) {
             alert('ERROR! You have entered a grid size larger than 100.');
             return;
+        } if (userInput === null) {
+            return;
+        } if (isNaN(userInput) || userInput == '') {
+            prompt('Please enter a valid number 1-100: ');
         }
         rows = userInput;
         columns = userInput;
         makeGrid(rows, columns);
-    })
 }
